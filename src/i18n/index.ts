d9e75a57455c397008236e9bbaeaa36fc83497fa
pauortega <pauortegarcia@outlook.es>
2024-03-07 04:52:00 +0100

    Añadir importaciones y traducciones en Intro.astro y ajustar estilos en PreFooter.astro

---
+++ src/i18n/index.ts
@@ -0,0 +1,17 @@
+import catalan from './ca.json';
+import spanish from './es.json';
+
+const LANGUAGES = {
+    CATALAN: 'ca',
+    SPANISH: 'es'
+};
+export const getI18N = ({ currentLocale = 'es' }: { currentLocale?: string | undefined }) => {
+  switch (currentLocale) {
+    case LANGUAGES.CATALAN:
+      return catalan;
+    case LANGUAGES.SPANISH:
+      return spanish;
+    default:
+      return spanish;
+  }
+}
